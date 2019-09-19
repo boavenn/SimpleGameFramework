c3ec3a6ec00ba6ee7698383ab2dd57eeb8533d95
Witek <brzozowskyy@gmail.com>
2019-09-19 16:54:32 +0200

    Added Box class

---
+++ Box.cpp
@@ -0,0 +1,112 @@
+#include "Box.h"
+
+Box::Box(const sf::Vector2f& size, const sf::Vector2f& pos)
+{
+	box.setSize(size);
+	box.setOrigin({ float(size.x / 2.f), float(size.y / 2.f) });
+	box.setPosition(pos);
+
+	sf::Color default_c = sf::Color::Green;
+	main_idle = default_c;
+	main_active = default_c;
+	outline_idle = default_c;
+	outline_active = default_c;
+	text_idle = default_c;
+	text_active = default_c;
+}
+
+void Box::draw(sf::RenderWindow& w)
+{
+	w.draw(box);
+	w.draw(text);
+}
+
+void Box::update(float dt, sf::Event e)
+{
+	// It may be unused as for now, it will be deleted eventually
+}
+
+void Box::setMainIdleColor(const sf::Color& c)
+{
+	main_idle = c;
+	updateMainColor(c);
+}
+
+void Box::setMainActiveColor(const sf::Color& c) 
+{ 
+	main_active = c; 
+}
+
+void Box::setOutlineIdleColor(const sf::Color& c)
+{
+	outline_idle = c;
+	updateOutlineColor(c);
+}
+
+void Box::setOutlineActiveColor(const sf::Color& c)
+{
+	outline_active = c;
+}
+
+void Box::setTextIdleColor(const sf::Color& c)
+{
+	text_idle = c;
+	updateTextColor(c);
+}
+
+void Box::setTextActiveColor(const sf::Color& c)
+{
+	text_active = c;
+}
+
+void Box::setFont(const std::string& font_name)
+{
+	font = ResourceManager::get().fonts.get(font_name);
+	text.setFont(font);
+}
+
+void Box::setText(const std::string& str, float spacing)
+{
+	text.setString(str);
+	text.setLetterSpacing(spacing);
+	updateTextSize();
+}
+
+void Box::setTexture(const std::string& tex_name)
+{
+	box.setTexture(&ResourceManager::get().textures.get(tex_name));
+	is_texture_set = true;
+}
+
+const sf::Vector2f& Box::getSize()
+{
+	return box.getSize();
+}
+
+const sf::Vector2f& Box::getPosition()
+{
+	return box.getPosition();
+}
+
+void Box::updateMainColor(const sf::Color& c)
+{
+	box.setFillColor(c);
+}
+
+void Box::updateOutlineColor(const sf::Color& c)
+{
+	box.setOutlineColor(c);
+}
+
+void Box::updateTextColor(const sf::Color& c)
+{
+	text.setFillColor(c);
+}
+
+void Box::updateTextSize()
+{
+	text.setCharacterSize(unsigned(box.getSize().y * 0.8f));
+	sf::FloatRect text_rect = text.getGlobalBounds();
+	text.setOrigin(text_rect.left + text_rect.width / 2.f, text_rect.top + text_rect.height / 2.f);
+	text.setPosition(box.getPosition());
+}
---
+++ Box.h
@@ -0,0 +1,38 @@
+#pragma once
+#include "ResourceManager.h"
+
+class Box
+{
+public:
+	Box(const sf::Vector2f& size, const sf::Vector2f& pos);
+	virtual void draw(sf::RenderWindow& w);
+	virtual void update(float dt, sf::Event e);
+
+	void setMainIdleColor(const sf::Color& c);
+	void setMainActiveColor(const sf::Color& c);
+	void setOutlineIdleColor(const sf::Color& c);
+	void setOutlineActiveColor(const sf::Color& c);
+	void setTextIdleColor(const sf::Color& c);
+	void setTextActiveColor(const sf::Color& c);
+	void setFont(const std::string& font_name);
+	void setText(const std::string& str, float spacing = 1.5f);
+	void setTexture(const std::string& tex_name);
+
+	const sf::Vector2f& getSize();
+	const sf::Vector2f& getPosition();
+
+protected:
+	void updateMainColor(const sf::Color& c);
+	void updateOutlineColor(const sf::Color& c);
+	void updateTextColor(const sf::Color& c);
+	void updateTextSize();
+
+	sf::RectangleShape box;
+	sf::Font font;
+	sf::Text text;
+	sf::Color main_idle, main_active;
+	sf::Color outline_idle, outline_active;
+	sf::Color text_idle, text_active;
+	bool is_texture_set = false;
+};
+
